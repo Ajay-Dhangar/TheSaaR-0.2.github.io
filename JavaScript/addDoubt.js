1b8c92986ecbecc98ff49b37c89ab0176ef70912
Triggered41 <vehdathamid@gmail.com>
2022-03-20 22:53:26 +0530

    aja-home.html -> added link to open about us and doubt section page doubt section page added to create a issue helper function in Javascript to create a new issue or retrieve issues from database

---
+++ JavaScript/addDoubt.js
@@ -0,0 +1,31 @@
+var searchField = document.getElementById('sf')
+var list = document.getElementById("doubtlist");
+
+doubtadd("MyIssue", "MyDescription");
+function create_issue()
+{
+    doubtadd("MyIssue", "MyDescription");
+}
+
+function doubtadd(issue_title, issue_description)
+{
+    var li = document.createElement("li")
+    var title = document.createElement("h3")
+    var username = document.createElement("h4")
+    var des = document.createElement("p")
+
+    title_text = document.createTextNode(issue_title)
+    username_text = document.createTextNode("MYUSERNAME")
+    des_text = document.createTextNode(issue_description)
+    
+    title.appendChild(title_text)
+    username.appendChild(username_text)
+    des.appendChild(des_text)
+
+    li.appendChild(username)
+    li.appendChild(title)
+    li.appendChild(des)
+
+    list.append(li)
+
+}
