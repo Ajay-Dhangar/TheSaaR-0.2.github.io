8ac247009dd7fa542be84fc97e11a90e4c368d3f
Triggered41 <vehdathamid@gmail.com>
2022-03-22 20:49:38 +0530

    basic blog page created

--- JavaScript/addDoubt.js
+++ JavaScript/addDoubt.js
@@ -1,30 +1,43 @@
 var searchField = document.getElementById('sf')
 var list = document.getElementById("doubtlist");
 
+
 function create_issue()
 {
-    doubtadd("MyIssue", "MyDescription");
+    doubtadd()
+    window.open("DoubtsSection.html", "_self")
 }
 
-function doubtadd(issue_title, issue_description)
+function doubtadd()
 {
-    var li = document.createElement("li")
-    var title = document.createElement("h3")
-    var username = document.createElement("h4")
-    var des = document.createElement("p")
+    var title = document.getElementsByName("name")[0].value; //title of the issue
+    var msg = document.getElementsByName("email-message")[0].value; // description of the issue
 
-    title_text = document.createTextNode(issue_title)
-    username_text = document.createTextNode("MYUSERNAME")
-    des_text = document.createTextNode(issue_description)
-    
-    title.appendChild(title_text)
-    username.appendChild(username_text)
-    des.appendChild(des_text)
-
-    li.appendChild(username)
-    li.appendChild(title)
-    li.appendChild(des)
-
-    list.append(li)
+    // Store it in MySQL using JavaScript or PHP
 
 }
+
+
+
+// ======================================================================================================
+
+// Do Not Delete This
+
+// var li = document.createElement("li")
+//     var title = document.createElement("h3")
+//     var username = document.createElement("h4")
+//     var des = document.createElement("p")
+
+//     title_text = document.createTextNode(issue_title)
+//     username_text = document.createTextNode("MYUSERNAME")
+//     des_text = document.createTextNode(issue_description)
+    
+//     title.appendChild(title_text)
+//     username.appendChild(username_text)
+//     des.appendChild(des_text)
+
+//     li.appendChild(username)
+//     li.appendChild(title)
+//     li.appendChild(des)
+
+//     list.append(li)
